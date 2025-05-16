c636174a7965929b3e27fe75bbb974a7b4181117
Oluwaferanmi-Dev <Oluwaferanmi@gmail.com>
2025-05-16 02:25:58 +0100

    added gallery images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,14 +4,14 @@ import { Button } from "@/components/ui/button";
 const Gallery = () => {
   // In a real application, these would be actual images
   const galleryImages = [
-    "/placeholder.svg",
-    "/placeholder.svg",
-    "/placeholder.svg",
-    "/placeholder.svg",
-    "/placeholder.svg",
-    "/placeholder.svg",
-    "/placeholder.svg",
-    "/placeholder.svg",
+    "/media/1b5b042f-2c90-4522-a56d-e14a74134b33.jpeg",
+    "/media/1cfd5f9c-0a32-4c27-8305-9473591bd624.jpeg",
+    "/media/9fc04fb2-4254-4818-81c2-ff613895737d.jpeg",
+    "/media/31dd079d-1e8e-4b7e-b0ed-da0e0a28cc89.jpeg",
+    "/media/a5a957da-125e-47fe-b226-dde0cd57c233.jpeg",
+    "/media/4933074e-840b-4031-baee-0a6f0d6ca0c2.jpeg",
+    "/media/d16acbe4-0a46-4623-8257-274e2b67653f.jpeg",
+    "/media/ae0ae72a-f824-4eed-947b-723de7df1b57.jpeg",
   ];
 
   return (
